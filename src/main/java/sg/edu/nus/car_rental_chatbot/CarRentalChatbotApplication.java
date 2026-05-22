2bfd56f9085ab75bd8db0ca9624a625413489f58
Qiao Xinyang <issv450@nus.edu.sg>
2026-05-22 12:59:58 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/car_rental_chatbot/CarRentalChatbotApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.car_rental_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/car_rental_chatbot/CarRentalChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.car_rental_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
